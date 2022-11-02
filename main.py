03fbc2c6306e46f18b272ded501d93dc65fa0e38
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-11-03 00:51:56 +0100

    added random clients

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from datetime import datetime
 
 from model import Validation, Client, Card
+import names
+
+
+def generate_random_clients(number_of_clients: int) -> set:
+    s = set()
+    for i in range(number_of_clients):
+        s.add(names.get_full_name())
+    print(s)
+    return s
 
 
 def main():
-    client = Client("4546er", "obaydah", "bouifadene")
-    card = Card("fes6", "subscription", datetime.now().isoformat())
-    validation = Validation(client, card, "chavant", "C", "condillac", datetime.now().isoformat())
-    print(validation.toJSON())
+    # client = Client("4546er", "obaydah", "bouifadene")
+    # card = Card("fes6", "subscription", datetime.now().isoformat())
+    # validation = Validation(client, card, "chavant", "C", "condillac", datetime.now().isoformat())
+    # print(validation.toJSON())
+    generate_random_clients(1000)
 
 
 if __name__ == '__main__':
