8b0eb2ca9014650f697d64b4e507359c0394bb1d
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-12-22 22:36:02 +0100

    added requirements

--- consumer.py
+++ consumer.py
@@ -8,6 +8,11 @@ from model import Validation
 
 consumer = KafkaConsumer(bootstrap_servers=kafka_endpoint)
 consumer.subscribe(topics=[kafka_topic])
+counter = 0
 for msg in consumer:
+    counter += 1
+    if counter == 8:
+        break
     v: Validation = json.loads(msg.value)
-    print(v["client"])
+    #print(v["client"])
+    print(v)
